926bf4791949d7c56499b75c858eb009afe29475
M <amfrombr@gmail.com>
2023-07-20 19:31:10 -0400

    ec2 stuff

--- feeder.js
+++ feeder.js
@@ -6,8 +6,8 @@ const prisma = new PrismaClient();
 
 async function etlProcess(filePath, modelName) {
   try {
-    const batchSize = 100; // Number of rows to process in each batch
-    let batch = []; // Array to accumulate rows for batch processing
+    const batchSize = 100;
+    let batch = [];
 
     fs.createReadStream(filePath)
       .pipe(csv())
@@ -22,7 +22,6 @@ async function etlProcess(filePath, modelName) {
 
         batch.push(transformedRow);
 
-        // Check if batch size is reached
         if (batch.length === batchSize) {
           console.log('Adding batch.');
           prisma[modelName].createMany({
@@ -33,12 +32,10 @@ async function etlProcess(filePath, modelName) {
               console.error('Error during batch insert:', error);
             });
 
-          // Clear batch array
           batch = [];
         }
       })
       .on('end', async () => {
-        // Insert remaining rows if any
         if (batch.length > 0) {
           prisma[modelName].createMany({
             data: batch,
